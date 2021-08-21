aa5361ec2239e89e4d8a761ef26d3393babb0ca9
TastSong <tastsong@163.com>
2021-08-21 15:50:13 +0800

    添加登录页面

--- CrazyCar/Assets/Scripts/UI/LoginUI.cs
+++ CrazyCar/Assets/Scripts/UI/LoginUI.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class LoginUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
