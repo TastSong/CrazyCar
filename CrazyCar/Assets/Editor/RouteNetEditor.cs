accd8afae89c0327a1ed8676f2bcbe0e7af79476
TastSong <tastsong@163.com>
2021-08-31 18:20:23 +0800

    加入读取EasyLoads路线的代码

---
+++ CrazyCar/Assets/Editor/RouteNetEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+using EasyRoads3Dv3;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(RouteNet))]
+public class RouteNetEditor : Editor
+{
+
+    private RouteNet rNet;
+
+    public ERRoad markers;
+
+    public override void OnInspectorGUI() {
+        rNet = target as RouteNet;
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+
+        if (GUILayout.Button("BuildAllRoute")) {
+            Undo.RecordObject(target, "BuildAllRoute");
+
+            ERRoadNetwork net = new ERRoadNetwork();
+            foreach (var r in rNet.allRoutes) {
+                Debug.Log("Road Name : " + r.__roadName + " Points Count " + r.positions.Count);
+                markers = net.GetRoadByName(r.__roadName);
+                if (markers == null) {
+                    Debug.LogError("NO SUCH ROAD.....CANCELED......");
+                    continue;
+                }
+
+                r.resolution = 50;
+                Vector3[] arr = markers.GetSplinePointsCenter();
+                r.positions = new List<Vector3>(arr);
+                for (int i = 0; i < r.positions.Count; i++) {
+                    //Debug.Log(r.positions[i]);
+                }
+            }
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
---
+++ CrazyCar/Assets/Scripts/Test/EasyLoadsTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyLoadsTest : MonoBehaviour {
+    public RouteManger routeManger = new RouteManger();
+    public RouteNet routeNet;
+
+    public float speed = 10;
+
+    public Rigidbody rig;
+    private float length;
+    private enum PosType {
+        LEFT = 0,
+        CENTER = 1,
+        RIGHT = 2
+    }
+
+    private PosType posType = PosType.CENTER;
+
+    private void Start() {
+        routeManger.InitPoint(routeNet.allRoutes[0].positions.ToArray());
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            posType = PosType.LEFT;
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            posType = PosType.CENTER;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            posType = PosType.RIGHT;
+        }
+
+        length += Time.deltaTime * speed;
+        if (length >= routeManger.centerPoints.Count - 1) {
+            length = routeManger.centerPoints.Count - 1;
+        }
+
+        if (posType == PosType.LEFT) {
+            rig.transform.localPosition = routeManger.leftPoints[(int)(length)];
+        } else if (posType == PosType.CENTER) {
+            rig.transform.localPosition = routeManger.centerPoints[(int)(length)];
+        } else {
+            rig.transform.localPosition = routeManger.rightPoints[(int)(length)];
+        }
+
+    }
+}
