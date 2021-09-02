0165329e3726fcf99a4b1ebf3ff60f17acd86d40
TastSong <tastsong@163.com>
2021-09-02 10:57:59 +0800

    主页添加车辆展示与控制

---
+++ CrazyCar/Assets/Scripts/Common/IndexCar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexCar : MonoBehaviour
+{
+    public static IndexCar manager = null;
+    public Transform carTrans;
+
+    private void Awake() {
+        if (manager == null) {
+            manager = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (manager != this) {
+            Destroy(gameObject);
+        }
+    }
+}
