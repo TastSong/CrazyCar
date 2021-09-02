d2117a3fe4365ea920a591d01c72e3964f78d887
TastSong <tastsong@163.com>
2021-09-02 10:06:14 +0800

    添加车辆模型

---
+++ CrazyCar/Assets/Prefabs/KaijuCar/Scripts/DemoTextureKaiju.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoTextureKaiju : MonoBehaviour
+{
+    public GUIStyle _text;
+
+    public Material _body;
+    public Material _self;
+    public Material _wheel;
+
+    public Texture[] _texture;
+    public Texture[] _textureOther;
+
+    public int _numTexture;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (_numTexture > 0)
+            {
+                _numTexture--;
+            }
+
+            _body.mainTexture = _texture[_numTexture];
+            _self.mainTexture = _textureOther[_numTexture];
+            _wheel.mainTexture = _textureOther[_numTexture];
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (_numTexture < _texture.Length - 1)
+            {
+                _numTexture++;
+            }
+
+            _body.mainTexture = _texture[_numTexture];
+            _self.mainTexture = _textureOther[_numTexture];
+            _wheel.mainTexture = _textureOther[_numTexture];
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, Screen.width, 100), "For view next texture press 'D' or 'A' for view back texture\n View texture: " + (_numTexture+1) + "/" + _texture.Length, _text);
+    }
+}
---
+++ CrazyCar/Assets/Prefabs/KaijuCar/Scripts/OrbitView.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitView : MonoBehaviour {
+
+    public Transform target;
+    public float distance = 10.0f;
+
+    public float xSpeed = 250.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -20;
+    public float yMaxLimit = 80;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    [AddComponentMenu("Camera-Control/Mouse Orbit")]
+   
+    // Use this for initialization
+    void Start () {
+        var angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().freezeRotation = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (target)
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+                var rotation = Quaternion.Euler(y, x, 0);
+                var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+
+                transform.rotation = rotation;
+                transform.position = position;
+            }
+        }
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
