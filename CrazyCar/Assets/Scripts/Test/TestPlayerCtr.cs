b34ff386332ea3178f3f0852052a4aa896fc0f01
TastSong <tastsong@163.com>
2021-09-05 19:41:30 +0800

    添加对Fantastic Nature Pack场景测试

---
+++ CrazyCar/Assets/Scripts/Test/TestPlayerCtr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerCtr : MonoBehaviour
+{
+    public float speed = 40;
+    public Rigidbody rig;
+
+    private void Update() {
+        //控制移动
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rig.MovePosition(transform.position + new Vector3(h, 0, v) * speed * Time.deltaTime);
+    }
+}
