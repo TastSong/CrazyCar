6f34cd12cf0e0907cbac241ad544c7993a7d9dbf
TastSong <tastsong@163.com>
2021-10-17 21:20:46 +0800

    添加Language UI

--- CrazyCar/Assets/Scripts/SystemSetting/SettingsUI.cs
+++ CrazyCar/Assets/Scripts/SystemSetting/SettingsUI.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Utils;
+using TMPro;
 
 public class SettingsUI : MonoBehaviour {
     public Button closeBtn;
+    public Button exitBtn;
+    public TMP_Dropdown languageDropdown;
 
     private void Start() {
         closeBtn.onClick.AddListener(() => {
             gameObject.SetActiveFast(false);
+        });
+
+        exitBtn.onClick.AddListener(() => {
+            Application.Quit();
         });
     }
 }
