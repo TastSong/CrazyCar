bb4ccad63a7b88e16b49c43fa2727cb088842591
TastSong <tastsong@163.com>
2021-11-07 22:01:01 +0800

    修改游戏开始倒计时屏幕特效

--- CrazyCar/Assets/Scripts/Anim/CountDownAnim.cs
+++ CrazyCar/Assets/Scripts/Anim/CountDownAnim.cs
@@ -15,23 +15,28 @@ public class CountDownAnim : MonoBehaviour {
 
     public void PlayAnim(int time, Action succ = null) {
         GameController.manager.StartCoroutine(CountDown(time));
-        ScreenEffectsManager.manager.wireframeProjector.gameObject.SetActiveFast(true);
+        PlayScreenEffect();
         Sequence sequence = DOTween.Sequence();
         for (int i = 0; i < time; i++) {
             sequence.Append(countDownText.transform.DOScale(1, 0.5f));
             sequence.Append(countDownText.transform.DOScale(0, 0.5f));
         }
-
-        Tween tween = DOTween.To(() => ScreenEffectsManager.manager.wireframeProjector.fieldOfView,
-            x => ScreenEffectsManager.manager.wireframeProjector.fieldOfView = x, 150, 4);
-        tween.OnComplete(() => {
-            ScreenEffectsManager.manager.wireframeProjector.gameObject.SetActiveFast(false);
-        });
-
         sequence.OnComplete(() => {
             succ?.Invoke();          
             gameObject.SetActiveFast(false);
         });       
+    }
+
+    private void PlayScreenEffect() {
+        ScreenEffectsManager.manager.wireframeProjector.gameObject.SetActiveFast(true);
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(DOTween.To(() => ScreenEffectsManager.manager.wireframeProjector.farClipPlane,
+            x => ScreenEffectsManager.manager.wireframeProjector.farClipPlane = x, 300, 3.4f));
+        sequence.Append(DOTween.To(() => ScreenEffectsManager.manager.wireframeProjector.farClipPlane,
+            x => ScreenEffectsManager.manager.wireframeProjector.farClipPlane = x, 0, 1.4f));
+        sequence.OnComplete(() => {
+            ScreenEffectsManager.manager.wireframeProjector.gameObject.SetActiveFast(false);
+        });
     }
 
     private IEnumerator CountDown(int time) {
