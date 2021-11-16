46e10e005dad5bb888b3c4986af6641bab5a8818
TastSong <tastsong@163.com>
2021-11-16 15:33:40 +0800

    添加对插件场景的测试

---
+++ CrazyCar/Assets/Plugins/Amazing Assets/Wireframe Shader/Example Scenes/Files/Scripts/AmazingAssetsLoadScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace AmazingAssets.WireframeShader.Example {
+    public class AmazingAssetsLoadScene : MonoBehaviour {
+        public static AmazingAssetsLoadScene manager = null;
+        public Button preBtn;
+        public Button nextBtn;
+
+        private void Awake() {
+            if (manager == null) {
+                DontDestroyOnLoad(gameObject);
+                manager = this;
+            } else if (manager != this) {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start() {
+            int count = SceneManager.sceneCountInBuildSettings;
+            preBtn.onClick.AddListener(() => {           
+                SceneManager.LoadSceneAsync((SceneManager.GetActiveScene().buildIndex - 1) % count);
+            }); 
+
+            nextBtn.onClick.AddListener(() => {              
+                SceneManager.LoadSceneAsync((SceneManager.GetActiveScene().buildIndex + 1) % count);
+            });
+        }
+    }
+}
