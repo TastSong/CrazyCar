82a09cea24ddd4479136a416276b9c49dcfe7dbd
TastSong <tastsong@163.com>
2021-11-25 14:05:23 +0800

    add PathRecorderUtility

--- CrazyCar/Assets/Scripts/Common/Util.cs
+++ CrazyCar/Assets/Scripts/Common/Util.cs
@@ -9,7 +9,6 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TFramework;
 
 namespace Utils {
     public static class GameObjectExtension {
@@ -20,45 +19,12 @@ namespace Utils {
         }
     }
 
-    public static class RequestUrl {
-        public static string forcedUpdatingUrl = "ForcedUpdating";
-        public static string loginUrl = "Login";
-        public static string registerUrl = "Register";
-        public static string avatarUrl = "Avatar";
-        public static string changeAvatarUrl = "ChangeAvatar";
-        public static string buyAvatarUrl = "BuyAvatar";
-        public static string resourceUrl = "Resource";
-        public static string timeTrialDetailUrl = "TimeTrialDetail";
-        public static string buyTimeTrialClassUrl = "BuyTimeTrialClass";
-        public static string timeTrialResultUrl = "TimeTrialResult";
-        public static string timeTrialRankUrl = "TimeTrialRank";
-        public static string modifyPersonalInfoUrl = "ModifyPersonalInfo";
-        public static string equipUrl = "Equip";
-        public static string buyEquipUrl = "BuyEquip";
-        public static string changeEquipUrl = "ChangeEquip";
-        public static string matchDetailUrl = "MatchDetail";
-        public static string matchResultUrl = "MatchResult";
-        public static string createMatchUrl = "CreateMatch";
-    }
-
-    public static class PrefKeys {
-        public const string lastLogNid = "lastLogNid";
-        public const string userName = "userName";
-        public const string password = "password";
-        public const string rememberPassword = "rememberPassword";
-    }
-
-    public static class LocalUrl {
-        public static string avatarUrl = "Avatar/";
-    }
-
     public enum SceneID {
         Login = 0,
         Index = 1,
         Loading = 2,
         Game = 3
     }
-
 
     public static class Util {
         public delegate void NoneParamFunction();
---
+++ CrazyCar/Assets/Scripts/Framework/Utility/PathRecorderUtility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TFramework;
+
+public static class RequestUrl {
+    public static string forcedUpdatingUrl = "ForcedUpdating";
+    public static string loginUrl = "Login";
+    public static string registerUrl = "Register";
+    public static string avatarUrl = "Avatar";
+    public static string changeAvatarUrl = "ChangeAvatar";
+    public static string buyAvatarUrl = "BuyAvatar";
+    public static string resourceUrl = "Resource";
+    public static string timeTrialDetailUrl = "TimeTrialDetail";
+    public static string buyTimeTrialClassUrl = "BuyTimeTrialClass";
+    public static string timeTrialResultUrl = "TimeTrialResult";
+    public static string timeTrialRankUrl = "TimeTrialRank";
+    public static string modifyPersonalInfoUrl = "ModifyPersonalInfo";
+    public static string equipUrl = "Equip";
+    public static string buyEquipUrl = "BuyEquip";
+    public static string changeEquipUrl = "ChangeEquip";
+    public static string matchDetailUrl = "MatchDetail";
+    public static string matchResultUrl = "MatchResult";
+    public static string createMatchUrl = "CreateMatch";
+}
+
+public static class PrefKeys {
+    public const string lastLogNid = "lastLogNid";
+    public const string userName = "userName";
+    public const string password = "password";
+    public const string rememberPassword = "rememberPassword";
+}
+
+public static class LocalUrl {
+    public static string avatarUrl = "Avatar/";
+}
