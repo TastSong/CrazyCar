1ecde65230fb8187473c6604c7a5dcfac6228ca7
TastSong <tastsong@163.com>
2021-12-15 14:43:06 +0800

    add KCP test

---
+++ CrazyCarServer/src/main/java/test/KCPTest.java
@@ -0,0 +1,95 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.backblaze.erasure.fec.Snmp;
+import io.netty.buffer.ByteBuf;
+import kcp.ChannelConfig;
+import kcp.KcpListener;
+import kcp.KcpServer;
+import kcp.Ukcp;
+
+/**
+ * Servlet implementation class KCPJavaWebServer
+ */
+public class KCPTest extends HttpServlet implements KcpListener {
+	private static final long serialVersionUID = 1L;
+
+	private boolean isInit = false;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public KCPTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if (!isInit) {
+			initKCP();
+			isInit = true;
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	private void initKCP() {
+		KCPTest kcpRttExampleServer = new KCPTest();
+
+        ChannelConfig channelConfig = new ChannelConfig();
+        channelConfig.nodelay(true,10,2,true);
+        channelConfig.setConv(40001);
+        channelConfig.setSndwnd(300);
+        channelConfig.setRcvwnd(300);
+        channelConfig.setMtu(512);
+        channelConfig.setAckNoDelay(true);
+        channelConfig.setTimeoutMillis(10000);
+        //channelConfig.setFecDataShardCount(10);
+        //channelConfig.setFecParityShardCount(3);
+        //c# crc32未实现
+        channelConfig.setCrc32Check(false);
+        KcpServer kcpServer = new KcpServer();
+        kcpServer.init(kcpRttExampleServer,channelConfig,40001);     
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	@Override
+    public void onConnected(Ukcp ukcp) {
+        System.out.println("Connected : "+Thread.currentThread().getName()+ukcp.user().getRemoteAddress());
+    }
+
+    @Override
+    public void handleReceive(ByteBuf buf, Ukcp kcp) {
+        byte[] bytes = new  byte[buf.readableBytes()];
+        buf.getBytes(buf.readerIndex(),bytes);
+        System.out.println("Receive: "+new String(bytes));
+        kcp.write(buf);
+    }
+
+    @Override
+    public void handleException(Throwable ex, Ukcp kcp) {
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void handleClose(Ukcp kcp) {
+        System.out.println("handleClose " + Snmp.snmp.toString());
+        Snmp.snmp  = new Snmp();
+    }
+
+}
