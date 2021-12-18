d76902a31f3992150020dc81d293766139914960
TastSong <tastsong@163.com>
2021-12-18 17:05:43 +0800

    KCP 无连接 无法直接广播

--- CrazyCarServer/src/main/java/CrazyCarServer/KCPRttServer.java
+++ CrazyCarServer/src/main/java/CrazyCarServer/KCPRttServer.java
@@ -2,12 +2,12 @@ package CrazyCarServer;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.alibaba.fastjson.JSONObject;
 import com.backblaze.erasure.fec.Snmp;
 
@@ -17,18 +17,14 @@ import kcp.ChannelConfig;
 import kcp.KcpListener;
 import kcp.KcpServer;
 import kcp.Ukcp;
+import test.KCPTest;
 
 /**
  * Servlet implementation class KCPServer
  */
 public class KCPRttServer extends HttpServlet implements KcpListener {
 	private static final long serialVersionUID = 1L;
-	
-	private Ukcp ukcp;
-	private boolean isInit = false; 	
-	private static int onlineCount = 0;
-    private static ConcurrentHashMap<String, KCPRttServer> kcpServerSet = new ConcurrentHashMap<String, KCPRttServer>();
-    private String id = "";
+	private boolean isInit = false;       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -43,7 +39,7 @@ public class KCPRttServer extends HttpServlet implements KcpListener {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html;charset=UTF-8");
-		System.out.println("Init KCP    1");
+		System.out.println("Init KCP");
 		String token = request.getHeader("Authorization");
 		if (!Util.JWT.isLegalJWT(token)){
 			System.out.println("illegal JWT");
@@ -66,7 +62,8 @@ public class KCPRttServer extends HttpServlet implements KcpListener {
 	}
 	
 	private void initKCP() {
-		KCPRttServer kcpRttServer = new KCPRttServer();
+		KCPTest kcpRttServer = new KCPTest();
+
         ChannelConfig channelConfig = new ChannelConfig();
         channelConfig.nodelay(true,10,2,true);
         channelConfig.setConv(40001);
@@ -88,40 +85,18 @@ public class KCPRttServer extends HttpServlet implements KcpListener {
 		doGet(request, response);
 	}
 	
-	private String uid;
 	@Override
     public void onConnected(Ukcp ukcp) {
-		uid = ukcp.user().getRemoteAddress().toString();
-        System.out.println("Connected : " + uid);
-        if (!kcpServerSet.values().contains(uid)) { 
-        	this.ukcp = ukcp;
-        	System.out.println("Connected : " + uid);
-	    	 kcpServerSet.put(uid, this);
-	    	 onlineCount++;
-        }  
+        System.out.println("Connected : "+Thread.currentThread().getName()+ukcp.user().getRemoteAddress());
     }
 
     @Override
-    public void handleReceive(ByteBuf buf, Ukcp uKcp) {
+    public void handleReceive(ByteBuf buf, Ukcp kcp) {
         byte[] bytes = new  byte[buf.readableBytes()];
         buf.getBytes(buf.readerIndex(),bytes);
-        //System.out.println("Receive: "+new String(bytes));
-        //kcp.write(buf);
-        if (!kcpServerSet.values().contains(uKcp.user().getRemoteAddress().toString())) { 
-        	this.ukcp = ukcp;
-        	System.out.println("Connected : " + uKcp.user().getRemoteAddress().toString());
-	    	 kcpServerSet.put(uKcp.user().getRemoteAddress().toString(), this);
-	    	 onlineCount++;
-        } 
-        for (String key : kcpServerSet.keySet()) {
-        	//System.out.println("Connected : " + kcpServerSet.get(key).user().getRemoteAddress().toString());
-            kcpServerSet.get(key).ukcp.write(buf);
-         } 
+        System.out.println("Receive: "+new String(bytes));
+        kcp.write(buf);
     }
-    
-    private void sendTOClient(String msg) {
-		
-	}
 
     @Override
     public void handleException(Throwable ex, Ukcp kcp) {
@@ -129,12 +104,8 @@ public class KCPRttServer extends HttpServlet implements KcpListener {
     }
 
     @Override
-    public void handleClose(Ukcp uKcp) {
+    public void handleClose(Ukcp kcp) {
         System.out.println("handleClose " + Snmp.snmp.toString());
-        if (kcpServerSet.values().contains(uKcp.user().getRemoteAddress().toString())) {
-       	 kcpServerSet.remove(uKcp.user().getRemoteAddress().toString());
-       	 onlineCount--;
-       } 
         Snmp.snmp  = new Snmp();
     }
 }
