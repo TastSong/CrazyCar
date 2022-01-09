9c9fe90b9dc4cba1d63f0fa778f14274efdda9b2
TastSong <tastsong@163.com>
2022-01-09 21:18:26 +0800

    add BuyAvatarCommand

--- CrazyCar/Assets/Scripts/Command/CommandHub.cs
+++ CrazyCar/Assets/Scripts/Command/CommandHub.cs
@@ -1,39 +1,9 @@
 ﻿using LitJson;
 using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
 using Utils;
 using QFramework;
 using MoreMountains.NiceVibrations;
-
-public class BuyAvatarCommand : AbstractCommand {
-    private AvatarInfo mAvatarInfo; 
-
-    public BuyAvatarCommand(AvatarInfo avatarInfo) {
-        mAvatarInfo = avatarInfo;
-    }
-
-    protected override void OnExecute() {
-        StringBuilder sb = new StringBuilder();
-        JsonWriter w = new JsonWriter(sb);
-        w.WriteObjectStart();
-        w.WritePropertyName("aid");
-        w.Write(mAvatarInfo.aid);
-        w.WriteObjectEnd();
-        Debug.Log("++++++ " + sb.ToString());
-        byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
-        CoroutineController.manager.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP
-            (url: this.GetSystem<INetworkSystem>().HttpBaseUrl + RequestUrl.buyAvatarUrl,
-        data: bytes,
-        token: this.GetModel<IGameControllerModel>().Token.Value,
-        succData: (data) => {
-            this.GetModel<IUserModel>().Star.Value = (int)data["star"];
-            mAvatarInfo.isHas = true;
-            this.GetModel<IUserModel>().AvatarNum.Value++;
-            this.SendEvent<UnlockAvatarEvent>();
-        }));
-    }
-}
 
 public class ApplyAvatarCommand : AbstractCommand {
     private readonly int mCurAid;
