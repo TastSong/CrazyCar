4122a4762e8c15769880548b01b7187cbc1c18a4
TastSong <tastsong@163.com>
2022-01-09 21:30:01 +0800

    add CreateMatchCommand

--- CrazyCar/Assets/Scripts/Command/CommandHub.cs
+++ CrazyCar/Assets/Scripts/Command/CommandHub.cs
@@ -5,20 +5,6 @@ using Utils;
 using QFramework;
 using MoreMountains.NiceVibrations;
 
-public class CreateMatchCommand : AbstractCommand {
-    protected override void OnExecute() {
-        CoroutineController.manager.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP(url: this.GetSystem<INetworkSystem>().HttpBaseUrl +
-                  RequestUrl.createMatchUrl,
-                  token: this.GetModel<IGameControllerModel>().Token.Value,
-                  code: (code) => {
-                      if (code == 200) {
-                          this.SendEvent<UpdataMatchDetailEvent>();
-                      }
-                  }
-              ));
-    }
-}
-
 public class ChangePasswordCommand : AbstractCommand {
     private string mPassword;
 
---
+++ CrazyCar/Assets/Scripts/Command/CreateMatchCommand.cs
@@ -0,0 +1,20 @@
+﻿using LitJson;
+using System.Text;
+using UnityEngine;
+using Utils;
+using QFramework;
+using MoreMountains.NiceVibrations;
+
+public class CreateMatchCommand : AbstractCommand {
+    protected override void OnExecute() {
+        CoroutineController.manager.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP(url: this.GetSystem<INetworkSystem>().HttpBaseUrl +
+                  RequestUrl.createMatchUrl,
+                  token: this.GetModel<IGameControllerModel>().Token.Value,
+                  code: (code) => {
+                      if (code == 200) {
+                          this.SendEvent<UpdataMatchDetailEvent>();
+                      }
+                  }
+              ));
+    }
+}
