d367defc52db2e97ee6f519b39f50f53a4073958
TastSong <tastsong@163.com>
2022-01-09 21:55:06 +0800

    add EnterTimeTrialCommand

--- CrazyCar/Assets/Scripts/Command/CommandHub.cs
+++ CrazyCar/Assets/Scripts/Command/CommandHub.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 using Utils;
 using QFramework;
 
-public class EnterTimeTrialCommand : AbstractCommand {
-    private TimeTrialInfo mTimeTrialInfo;
-
-    public EnterTimeTrialCommand(TimeTrialInfo timeTrialInfo) {
-        mTimeTrialInfo = timeTrialInfo;
-    }
-
-    protected override void OnExecute() {
-        Debug.Log("进入课程 = " + mTimeTrialInfo.cid);
-        this.GetModel<ITimeTrialModel>().CleanData();
-        this.GetModel<ITimeTrialModel>().SelectInfo.Value = mTimeTrialInfo;
-        this.GetModel<IGameControllerModel>().CurGameType = GameType.TimeTrial;
-        Util.LoadingScene(SceneID.Game);
-    }
-}
-
 public class BuyTimeTrialClassCommand : AbstractCommand {
     private TimeTrialInfo mTimeTrialInfo;
 
