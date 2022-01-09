9ad68fb58ef586bcba6f6a9b02d68f983f328dd9
TastSong <tastsong@163.com>
2022-01-09 21:42:19 +0800

    add LoginCommand

---
+++ CrazyCar/Assets/Scripts/Command/LoginCommand.cs
@@ -0,0 +1,53 @@
+﻿using LitJson;
+using System.Text;
+using UnityEngine;
+using Utils;
+using QFramework;
+using MoreMountains.NiceVibrations;
+
+public class LoginCommand : AbstractCommand {
+    private string mUserName;
+    private string mPassword;
+    private bool mIsRemember;
+
+    public LoginCommand(string userName, string password, bool isRemember) {
+        mUserName = userName;
+        mPassword = password;
+        mIsRemember = isRemember;
+    }
+
+    protected override void OnExecute() {
+        StringBuilder sb = new StringBuilder();
+        JsonWriter w = new JsonWriter(sb);
+        w.WriteObjectStart();
+        w.WritePropertyName("UserName");
+        w.Write(mUserName);
+        w.WritePropertyName("Password");
+        w.Write(mPassword);
+        w.WriteObjectEnd();
+        Debug.Log("++++++ " + sb.ToString());
+        byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
+        CoroutineController.manager.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP(url: this.GetSystem<INetworkSystem>().HttpBaseUrl + RequestUrl.loginUrl,
+            data: bytes, succData: (data) => {
+                this.GetModel<IGameControllerModel>().Token.Value = (string)data["token"];
+                this.GetSystem<IDataParseSystem>().ParseUserInfo(data);
+                this.GetModel<IUserModel>().Password.Value = mPassword;
+            }, code: (code) => {
+                if (code == 200) {
+                    this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(text: this.GetSystem<II18NSystem>().GetText("Login Success"),
+                        callback: () => {
+                            this.GetSystem<IVibrationSystem>().Haptic(HapticTypes.Success);
+                            this.GetModel<IUserModel>().RememberPassword.Value = mIsRemember ? 1 : 0;
+                            Util.LoadingScene(SceneID.Index);
+                        });
+
+                } else if (code == 423) {
+                    this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("Password Error"));
+                } else if (code == 404) {
+                    this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("User not registered"));
+                } else {
+                    this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("Unknown Error"));
+                }
+            }));
+    }
+}
