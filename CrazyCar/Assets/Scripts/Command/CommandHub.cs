25fee47d8beb4d9a63ada6ef8eb4722c749a1e23
TastSong <tastsong@163.com>
2022-01-09 21:52:28 +0800

    add EnterMatchCommand

--- CrazyCar/Assets/Scripts/Command/CommandHub.cs
+++ CrazyCar/Assets/Scripts/Command/CommandHub.cs
@@ -3,30 +3,6 @@ using System.Text;
 using UnityEngine;
 using Utils;
 using QFramework;
-using MoreMountains.NiceVibrations;
-public class EnterMatchCommand : AbstractCommand {
-    private MatchInfo mMatchInfo;
-    
-    public EnterMatchCommand(MatchInfo matchInfo) {
-        mMatchInfo = matchInfo;
-    }
-
-    protected override void OnExecute() {
-        if (CanEnter(mMatchInfo)) {
-            Debug.Log("进入课程 = " + mMatchInfo.cid);
-            this.GetModel<IMatchModel>().CleanData();
-            this.GetModel<IMatchModel>().SelectInfo.Value = mMatchInfo;
-            this.GetModel<IGameControllerModel>().CurGameType = GameType.Match;
-            Util.LoadingScene(SceneID.Game);
-        } else {
-            this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("The game is over"));
-        }
-    }
-
-    private bool CanEnter(MatchInfo matchInfo) {
-        return (matchInfo.startTime + matchInfo.limitTime) * 1000 > Util.GetTime();
-    }
-}
 
 public class EnterTimeTrialCommand : AbstractCommand {
     private TimeTrialInfo mTimeTrialInfo;
---
+++ CrazyCar/Assets/Scripts/Command/EnterMatchCommand.cs
@@ -0,0 +1,28 @@
+﻿using LitJson;
+using System.Text;
+using UnityEngine;
+using Utils;
+using QFramework;
+public class EnterMatchCommand : AbstractCommand {
+    private MatchInfo mMatchInfo;
+
+    public EnterMatchCommand(MatchInfo matchInfo) {
+        mMatchInfo = matchInfo;
+    }
+
+    protected override void OnExecute() {
+        if (CanEnter(mMatchInfo)) {
+            Debug.Log("进入课程 = " + mMatchInfo.cid);
+            this.GetModel<IMatchModel>().CleanData();
+            this.GetModel<IMatchModel>().SelectInfo.Value = mMatchInfo;
+            this.GetModel<IGameControllerModel>().CurGameType = GameType.Match;
+            Util.LoadingScene(SceneID.Game);
+        } else {
+            this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("The game is over"));
+        }
+    }
+
+    private bool CanEnter(MatchInfo matchInfo) {
+        return (matchInfo.startTime + matchInfo.limitTime) * 1000 > Util.GetTime();
+    }
+}
