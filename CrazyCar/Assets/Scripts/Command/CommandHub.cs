42c2020c6e8e91c1207828eca5a2f555a1c37fc8
TastSong <tastsong@163.com>
2022-01-09 21:38:50 +0800

    add EnableStandAloneCommand.

--- CrazyCar/Assets/Scripts/Command/CommandHub.cs
+++ CrazyCar/Assets/Scripts/Command/CommandHub.cs
@@ -5,21 +5,6 @@ using Utils;
 using QFramework;
 using MoreMountains.NiceVibrations;
 
-public class EnableStandAloneCommand : AbstractCommand {
-    protected override void OnExecute() {
-        this.GetModel<IGameControllerModel>().StandAlone.Value = true;
-        TextAsset ta = Resources.Load<TextAsset>(Util.baseStandAlone + RequestUrl.loginUrl);
-        JsonData data = JsonMapper.ToObject(ta.text);
-        this.GetModel<IGameControllerModel>().Token.Value = (string)data["token"];
-        this.GetSystem<IDataParseSystem>().ParseUserInfo(data);
-
-        this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(text: this.GetSystem<II18NSystem>().GetText("Login Success"),
-            callback: () => {
-                Util.LoadingScene(SceneID.Index);
-            });
-    }
-}
-
 public class LoginCommand : AbstractCommand {
     private string mUserName;
     private string mPassword;
---
+++ CrazyCar/Assets/Scripts/Command/EnableStandAloneCommand.cs
@@ -0,0 +1,21 @@
+﻿using LitJson;
+using System.Text;
+using UnityEngine;
+using Utils;
+using QFramework;
+using MoreMountains.NiceVibrations;
+
+public class EnableStandAloneCommand : AbstractCommand {
+    protected override void OnExecute() {
+        this.GetModel<IGameControllerModel>().StandAlone.Value = true;
+        TextAsset ta = Resources.Load<TextAsset>(Util.baseStandAlone + RequestUrl.loginUrl);
+        JsonData data = JsonMapper.ToObject(ta.text);
+        this.GetModel<IGameControllerModel>().Token.Value = (string)data["token"];
+        this.GetSystem<IDataParseSystem>().ParseUserInfo(data);
+
+        this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(text: this.GetSystem<II18NSystem>().GetText("Login Success"),
+            callback: () => {
+                Util.LoadingScene(SceneID.Index);
+            });
+    }
+}
