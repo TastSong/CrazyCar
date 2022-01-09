6ce5e073080df0faf5d6e832accfebb8f5fd5825
TastSong <tastsong@163.com>
2022-01-09 22:00:27 +0800

    add SavaSettingsCommand

--- CrazyCar/Assets/Scripts/Command/SavaSettingsCommand.cs
+++ CrazyCar/Assets/Scripts/Command/SavaSettingsCommand.cs
@@ -1,11 +1,8 @@
-﻿using LitJson;
-using System.Text;
-using UnityEngine;
-using Utils;
-using QFramework;
+﻿using QFramework;
 
 public class SavaSettingsCommand : AbstractCommand {
     protected override void OnExecute() {
         this.SendEvent<ChangeSettingEvent>();
     }
 }
+
