3177d66cb1288a5b8ed05afd909fade95f1c6587
TastSong <tastsong@163.com>
2022-01-09 21:21:21 +0800

    add ApplyAvatarCommand

---
+++ CrazyCar/Assets/Scripts/Command/ApplyAvatarCommand.cs
@@ -0,0 +1,37 @@
+﻿using LitJson;
+using System.Text;
+using UnityEngine;
+using Utils;
+using QFramework;
+using MoreMountains.NiceVibrations;
+
+public class ApplyAvatarCommand : AbstractCommand {
+    private readonly int mCurAid;
+
+    public ApplyAvatarCommand(int curAid) {
+        mCurAid = curAid;
+    }
+
+    protected override void OnExecute() {
+        StringBuilder sb = new StringBuilder();
+        JsonWriter w = new JsonWriter(sb);
+        w.WriteObjectStart();
+        w.WritePropertyName("aid");
+        w.Write(mCurAid);
+        w.WriteObjectEnd();
+        Debug.Log("++++++ " + sb.ToString());
+        byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
+        CoroutineController.manager.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP(url: this.GetSystem<INetworkSystem>().HttpBaseUrl + RequestUrl.changeAvatarUrl,
+            data: bytes, token: this.GetModel<IGameControllerModel>().Token.Value,
+            succData: (data) => {
+                this.GetModel<IUserModel>().Aid.Value = (int)data["aid"];
+                this.SendEvent(new UpdataAvatarUIEvent(this.GetModel<IUserModel>().Aid));
+                this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("Successfully Set"));
+            },
+            code: (code) => {
+                if (code == 423) {
+                    this.GetModel<IGameControllerModel>().WarningAlert.ShowWithText(this.GetSystem<II18NSystem>().GetText("Did not have"));
+                }
+            }));
+    }
+}
