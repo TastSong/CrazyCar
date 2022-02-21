9937984fe8c48a7c2d6421be4d246d125d69284c
TastSong <tastsong@163.com>
2022-02-21 16:54:37 +0800

    添加道具

---
+++ CrazyCar/Assets/Scripts/Game/PropController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFramework;
+using Utils;
+
+public enum PropType {
+    SpeedUp,
+    SpeedDown
+}
+
+public class PropController : MonoBehaviour, IController {
+    public PropType propType = PropType.SpeedUp;
+    public float time = 1;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            if (propType == PropType.SpeedUp) {
+                this.SendCommand(new PlayerControllerCommand(ControllerType.Speed, 1));
+                this.GetSystem<IPlayerManagerSystem>().SelfPlayer.driftLevel = DriftLevel.Three;
+                this.SendCommand(new PlayerControllerCommand(ControllerType.Speed, -1));
+            }
+        }
+    }
+
+    public IArchitecture GetArchitecture() {
+        return CrazyCar.Interface;
+    }
+}
