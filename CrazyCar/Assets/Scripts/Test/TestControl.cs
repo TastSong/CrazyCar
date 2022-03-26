e525406328a01c287d0f69e045503548f2adacd0
TastSong <tastsong@163.com>
2022-03-26 22:03:59 +0800

    test control

---
+++ CrazyCar/Assets/Scripts/Test/TestControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControl : MonoBehaviour{
+    private Rigidbody rig;
+
+    private void Start() {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rig.velocity = new Vector3(20, 0, 10);
+        }
+    }
+}
