6d9c36f8dbbdbc7afe5694b74c77631d9809c0e1
TastSong <tastsong@163.com>
2022-04-05 15:45:21 +0800

    修改warning alert

--- CrazyCar/Assets/Scripts/UI/WarningAlert.cs
+++ CrazyCar/Assets/Scripts/UI/WarningAlert.cs
@@ -3,30 +3,43 @@ using UnityEngine;
 using UnityEngine.UI;
 using Utils;
 using System;
+using System.Collections.Generic;
 
 public class WarningAlert : MonoBehaviour {
     public Text warningText;
+
+    private class WarningAlertInfo{
+        public string text;
+        public float time;
+        public Action callback;
+
+        public WarningAlertInfo(string content, float t, Action action) {
+            text = content;
+            time = t;
+            callback = action;
+        }
+    }
+    private Queue<WarningAlertInfo> queue = new Queue<WarningAlertInfo>();
     private Coroutine warningCor;
 
 
     public void ShowWithText(string text, float time = 1.0f, Action callback = null) {
+        WarningAlertInfo info = new WarningAlertInfo(text, time, callback);
+        queue.Enqueue(info);
         gameObject.SetActiveFast(true);
-        warningText.text = text;
-        if (warningCor != null) {
-            StopCoroutine(warningCor);
+        if (warningCor == null) {
+            warningCor = StartCoroutine(HideWarningAlert());
+        }       
+    }
+
+    private IEnumerator HideWarningAlert() {
+        while (queue.Count != 0) {
+            WarningAlertInfo info = queue.Dequeue();
+            warningText.text = info.text;
+            yield return new WaitForSeconds(info.time);           
+            info.callback?.Invoke();
         }
-
-        warningCor = StartCoroutine(HideWarningAlert(time, callback));
-    }
-
-    private IEnumerator HideWarningAlert(float time, Action callback = null) {
-        yield return new WaitForSeconds(time);
+        warningCor = null;
         gameObject.SetActiveFast(false);
-        callback?.Invoke();
-    }
-
-    public void Hide() {
-        gameObject.SetActiveFast(false);
-        warningText.text = "";
     }
 }
