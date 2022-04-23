19dc1e2f2bacd2a6a1e33532f39cd829784e1bcd
TastSong <tastsong@163.com>
2022-04-23 15:05:35 +0800

    add PostPlayerPosMsgCommand

---
+++ CrazyCar/Assets/Scripts/Command/PostPlayerPosMsgCommand.cs
@@ -0,0 +1,54 @@
+using LitJson;
+using System.Text;
+using Utils;
+using QFramework;
+
+
+public class PostPlayerPosMsgCommand : AbstractCommand{
+    protected override void OnExecute() {
+        StringBuilder sb = new StringBuilder();
+            JsonWriter w = new JsonWriter(sb);
+            w.WriteObjectStart();
+            w.WritePropertyName("cid");
+            if (this.GetModel<IGameControllerModel>().CurGameType == GameType.Match) {
+                w.Write(this.GetModel<IMatchModel>().SelectInfo.Value.cid);
+            } else {
+                w.Write(this.GetModel<ITimeTrialModel>().SelectInfo.Value.cid);
+            }
+            w.WritePropertyName("pos_x");
+            w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.x);
+            w.WritePropertyName("pos_y");
+            w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.y);
+            w.WritePropertyName("pos_z");
+            w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.z);
+            w.WritePropertyName("speed");
+            w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.x.ToString("f2") + "," + 
+                this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.y.ToString("f2") + "," +
+                this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.z.ToString("f2"));
+            w.WritePropertyName("timestamp");
+            w.Write(Util.GetTime());
+            w.WritePropertyName("user_info");
+            w.WriteObjectStart();
+            w.WritePropertyName("name");
+            w.Write(this.GetModel<IUserModel>().Name.Value);
+            w.WritePropertyName("uid");
+            w.Write(this.GetModel<IUserModel>().Uid.Value);
+            w.WritePropertyName("aid");
+            w.Write(this.GetModel<IUserModel>().Aid.Value);
+            w.WritePropertyName("star");
+            w.Write(this.GetModel<IUserModel>().Star.Value);
+            w.WritePropertyName("is_vip");
+            w.Write(this.GetModel<IUserModel>().IsVIP.Value);
+            w.WritePropertyName("equip_info");
+            w.WriteObjectStart();
+            w.WritePropertyName("eid");
+            w.Write(this.GetModel<IUserModel>().EquipInfo.Value.eid);
+            w.WritePropertyName("rid");
+            w.Write(this.GetModel<IUserModel>().EquipInfo.Value.rid);
+            w.WriteObjectEnd();
+            w.WriteObjectEnd();
+            w.WriteObjectEnd();
+            //Debug.Log("Post Server : " + sb.ToString());
+            this.GetSystem<INetworkSystem>().SendMsgToServer(sb.ToString());
+    }
+}
--- CrazyCar/Assets/Scripts/UI/TimeTrial/TimeTrialNet.cs
+++ CrazyCar/Assets/Scripts/UI/TimeTrial/TimeTrialNet.cs
@@ -25,46 +25,7 @@ public class TimeTrialNet : MonoBehaviour, IController {
     private IEnumerator SendMsg() {
         while (true) {
             if (this.GetModel<ITimeTrialModel>().IsStartGame) {
-                StringBuilder sb = new StringBuilder();
-                JsonWriter w = new JsonWriter(sb);
-                w.WriteObjectStart();
-                w.WritePropertyName("cid");
-                w.Write(this.GetModel<IMatchModel>().SelectInfo.Value.cid);
-                w.WritePropertyName("pos_x");
-                w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.x);
-                w.WritePropertyName("pos_y");
-                w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.y);
-                w.WritePropertyName("pos_z");
-                w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.transform.position.z);
-                w.WritePropertyName("speed");
-                w.Write(this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.x.ToString("f2") + "," +
-                    this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.y.ToString("f2") + "," +
-                    this.GetSystem<IPlayerManagerSystem>().SelfPlayer.rig.velocity.z.ToString("f2"));
-                w.WritePropertyName("timestamp");
-                w.Write(Util.GetTime());
-                w.WritePropertyName("user_info");
-                w.WriteObjectStart();
-                w.WritePropertyName("name");
-                w.Write(this.GetModel<IUserModel>().Name.Value);
-                w.WritePropertyName("uid");
-                w.Write(this.GetModel<IUserModel>().Uid.Value);
-                w.WritePropertyName("aid");
-                w.Write(this.GetModel<IUserModel>().Aid.Value);
-                w.WritePropertyName("star");
-                w.Write(this.GetModel<IUserModel>().Star.Value);
-                w.WritePropertyName("is_vip");
-                w.Write(this.GetModel<IUserModel>().IsVIP.Value);
-                w.WritePropertyName("equip_info");
-                w.WriteObjectStart();
-                w.WritePropertyName("eid");
-                w.Write(this.GetModel<IUserModel>().EquipInfo.Value.eid);
-                w.WritePropertyName("rid");
-                w.Write(this.GetModel<IUserModel>().EquipInfo.Value.rid);
-                w.WriteObjectEnd();
-                w.WriteObjectEnd();
-                w.WriteObjectEnd();
-                Debug.Log("Post Server : " + sb.ToString());
-                this.GetSystem<INetworkSystem>().SendMsgToServer(sb.ToString());
+                this.SendCommand<PostPlayerPosMsgCommand>();
             }
             yield return new WaitForSeconds(this.GetModel<IGameControllerModel>().SendMsgOffTime.Value);
         }
