bdfcbfa819c3ce66c654af23dbaed778b39fb956
TastSong <tastsong@163.com>
2022-04-25 13:43:34 +0800

    进行新手教程测试

--- CrazyCar/Assets/Scripts/Guidance/GuidanceEventPenetrate.cs
+++ CrazyCar/Assets/Scripts/Guidance/GuidanceEventPenetrate.cs
@@ -1,21 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class GuidanceEventPenetrate : MonoBehaviour, ICanvasRaycastFilter
-{
+public class GuidanceEventPenetrate : MonoBehaviour, IPointerClickHandler {
+	public int index = 0;
+    public int maxIndex = 0;
+
 	private Image _targetImage;
 
 	public void SetTargetImage(Image target)
 	{
 		_targetImage = target;
 	}
-	public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
-	{
-		if (_targetImage == null)
-			return true;
 
-		return !RectTransformUtility.RectangleContainsScreenPoint(_targetImage.rectTransform, sp, eventCamera);
-	}
+    public void OnPointerClick(PointerEventData eventData) {
+        if (!RectTransformUtility.RectangleContainsScreenPoint(_targetImage.rectTransform, Input.mousePosition, Camera.main)) {
+            index = Mathf.Min(index + 1, maxIndex);
+			Psss(eventData, ExecuteEvents.pointerClickHandler);
+		} 
+    }
+
+    private bool hasPassedEvent = false;
+    public void Psss<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function)
+        where T : IEventSystemHandler {
+        if (hasPassedEvent) {
+            return;
+        }
+        hasPassedEvent = true;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+        GameObject current = data.pointerCurrentRaycast.gameObject;
+
+        for (int i = 0; i < results.Count; i++) {
+            Debug.LogError("++++ " + results[i].gameObject.name);
+            if (current != results[i].gameObject) {
+                if (ExecuteEvents.Execute(results[i].gameObject, data, function)) {
+                    break;
+                }
+            }
+        }
+        results.Clear();
+        hasPassedEvent = false;
+    }
 }
--- CrazyCar/Assets/Scripts/Guidance/RectGuidanceController.cs
+++ CrazyCar/Assets/Scripts/Guidance/RectGuidanceController.cs
@@ -3,68 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// 矩形引导组件
-/// </summary>
 public class RectGuidanceController : MonoBehaviour
 {
+	public Image[] Target;
+	public Button but;
 
-	/// <summary>
-	/// 高亮显示的目标
-	/// </summary>
-	public Image Target;
+	public int index = 0;
 	
-	/// <summary>
-	/// 区域范围缓存
-	/// </summary>
+	// 区域范围缓存
 	private Vector3[] _corners = new Vector3[4];
 
-	/// <summary>
-	/// 镂空区域中心
-	/// </summary>
+	// 镂空区域中心
 	private Vector4 _center;
 
-	/// <summary>
-	/// 最终的偏移值X
-	/// </summary>
 	private float _targetOffsetX = 0f;
-
-	/// <summary>
-	/// 最终的偏移值Y
-	/// </summary>
 	private float _targetOffsetY = 0f;
 
-	/// <summary>
-	/// 遮罩材质
-	/// </summary>
+	// 遮罩材质
 	private Material _material;
 	
-	/// <summary>
-	/// 当前的偏移值X
-	/// </summary>
 	private float _currentOffsetX = 0f;
-
-	/// <summary>
-	/// 当前的偏移值Y
-	/// </summary>
 	private float _currentOffsetY = 0f;
 
-	/// <summary>
-	/// 动画收缩时间
-	/// </summary>
 	private float _shrinkTime = 0.5f;
-
-	/// <summary>
-	/// 时间渗透组件
-	/// </summary>
 	private GuidanceEventPenetrate _eventPenetrate;
 
-	/// <summary>
-	/// 世界坐标到画布坐标的转换
-	/// </summary>
-	/// <param name="canvas">画布</param>
-	/// <param name="world">世界坐标</param>
-	/// <returns>转换后在画布的坐标</returns>
 	private Vector2 WorldToCanvasPos(Canvas canvas, Vector3 world)
 	{
 		Vector2 position;
@@ -76,33 +39,42 @@ public class RectGuidanceController : MonoBehaviour
 	private void Awake()
 	{
 		_eventPenetrate = GetComponent<GuidanceEventPenetrate>();
-		if(_eventPenetrate != null)
-			_eventPenetrate.SetTargetImage(Target);
-		//获取画布
+
+		_eventPenetrate.maxIndex = (Target.Length - 1);
+		if (Target.Length > 0) {
+			SetTaget(Target[0]);
+		}
+
+		but.onClick.AddListener(() => {
+			Debug.LogError("_+++++ 点击");
+		});
+	}
+
+	private void SetTaget(Image image) {
+		if (_eventPenetrate != null)
+			_eventPenetrate.SetTargetImage(image);
 		Canvas canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
 		//获取高亮区域四个顶点的世界坐标
-		Target.rectTransform.GetWorldCorners(_corners);
+		image.rectTransform.GetWorldCorners(_corners);
 		//计算高亮显示区域咋画布中的范围
 		_targetOffsetX = Vector2.Distance(WorldToCanvasPos(canvas, _corners[0]), WorldToCanvasPos(canvas, _corners[3])) / 2f;
 		_targetOffsetY = Vector2.Distance(WorldToCanvasPos(canvas, _corners[0]), WorldToCanvasPos(canvas, _corners[1])) / 2f;
 		//计算高亮显示区域的中心
 		float x = _corners[0].x + ((_corners[3].x - _corners[0].x) / 2f);
 		float y = _corners[0].y + ((_corners[1].y - _corners[0].y) / 2f);
-		Vector3 centerWorld = new Vector3(x,y,0);
+		Vector3 centerWorld = new Vector3(x, y, 0);
 		Vector2 center = WorldToCanvasPos(canvas, centerWorld);
 		//设置遮罩材料中中心变量
-		Vector4 centerMat = new Vector4(center.x,center.y,0,0);
+		Vector4 centerMat = new Vector4(center.x, center.y, 0, 0);
 		_material = GetComponent<Image>().material;
-		_material.SetVector("_Center",centerMat);
+		_material.SetVector("_Center", centerMat);
 		//计算当前偏移的初始值
 		RectTransform canvasRectTransform = (canvas.transform as RectTransform);
-		if (canvasRectTransform != null)
-		{
+		if (canvasRectTransform != null) {
 			//获取画布区域的四个顶点
 			canvasRectTransform.GetWorldCorners(_corners);
 			//求偏移初始值
-			for (int i = 0; i < _corners.Length; i++)
-			{
+			for (int i = 0; i < _corners.Length; i++) {
 				if (i % 2 == 0)
 					_currentOffsetX = Mathf.Max(Vector3.Distance(WorldToCanvasPos(canvas, _corners[i]), center), _currentOffsetX);
 				else
@@ -110,8 +82,8 @@ public class RectGuidanceController : MonoBehaviour
 			}
 		}
 		//设置遮罩材质中当前偏移的变量
-		_material.SetFloat("_SliderX",_currentOffsetX);
-		_material.SetFloat("_SliderY",_currentOffsetY);
+		_material.SetFloat("_SliderX", _currentOffsetX);
+		_material.SetFloat("_SliderY", _currentOffsetY);
 	}
 
 	private float _shrinkVelocityX = 0f;
@@ -133,5 +105,10 @@ public class RectGuidanceController : MonoBehaviour
 			_currentOffsetY = valueY;
 			_material.SetFloat("_SliderY",_currentOffsetY);
 		}
+
+		if (_eventPenetrate != null && _eventPenetrate.index != index) {
+			SetTaget(Target[_eventPenetrate.index]);
+			index = _eventPenetrate.index;
+        }
 	}
 }
