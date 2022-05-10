411420210dd8dbbd84eee0bf0dc7e565e04a767c
TastSong <tastsong@163.com>
2022-05-10 10:52:37 +0800

    update database

--- CrazyCarDB/CrazyCar.sql
+++ CrazyCarDB/CrazyCar.sql
@@ -329,13 +329,13 @@ create table if not exists `forced_updating`(
    )engine = innodb default charset = utf8;
 insert into forced_updating ( platform, version, rule, url)
 				   values
-				   ("Android", "8.0.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");
+				   ("Android", "8.1.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");
 insert into forced_updating ( platform, version, rule, url)
 				   values
-				   ("ios", "8.0.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");
+				   ("ios", "8.1.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");
 insert into forced_updating ( platform, version, rule, url)
 				   values
-				   ("PC", "8.0.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");                   
+				   ("PC", "8.1.0", 10000, "https://github.com/TastSong/CrazyCar/releases/latest");                   
 select * from forced_updating;
 
 /*all_equip*/
