6b20876267ed4f35ce4637ad4f86a160087d40ec
TastSong <tastsong@163.com>
2022-06-27 10:08:03 +0800

    init spring boot server

---
+++ CarzyCarSpringBootServer/crazycar/src/main/java/com/tastsong/crazycar/CrazycarApplication.java
@@ -0,0 +1,13 @@
+package com.tastsong.crazycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazycarApplication.class, args);
+	}
+
+}
---
+++ CarzyCarSpringBootServer/crazycar/src/test/java/com/tastsong/crazycar/CrazycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastsong.crazycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
