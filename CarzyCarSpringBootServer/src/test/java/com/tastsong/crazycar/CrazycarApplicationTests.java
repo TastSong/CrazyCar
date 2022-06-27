3f392ee602a76efeea84b89a1f3bd133588aa9ee
TastSong <tastsong@163.com>
2022-06-27 10:12:19 +0800

    调整spring boot server目录结构

--- CarzyCarSpringBootServer/src/test/java/com/tastsong/crazycar/CrazycarApplicationTests.java
+++ CarzyCarSpringBootServer/src/test/java/com/tastsong/crazycar/CrazycarApplicationTests.java
@@ -3,7 +3,7 @@ package com.tastsong.crazycar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CrazycarApplicationTests {
 
 	@Test
