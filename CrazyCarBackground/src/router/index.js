71fb39d6ec25561b53c24a1b0c81efe02c9792f9
TastSong <tastsong@163.com>
2022-07-20 15:57:12 +0800

    change link

--- CrazyCarBackground/src/router/index.js
+++ CrazyCarBackground/src/router/index.js
@@ -154,7 +154,7 @@ export const constantRoutes = [
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        path: 'https://github.com/TastSong/CrazyCar',
         meta: { title: 'External Link', icon: 'link' }
       }
     ]
--- CrazyCarBackground/vue.config.js
+++ CrazyCarBackground/vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
+const name = defaultSettings.title || 'Crazy Car Background' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
