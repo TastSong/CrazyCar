c4ab1a62ca0c31d89072d9b09696bd15517b9cf5
TastSong <tastsong@163.com>
2022-07-21 16:52:59 +0800

    BackgroundDashboardController fake data

--- CarzyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundDashboardController.java
+++ CarzyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundDashboardController.java
@@ -17,16 +17,40 @@ public class BackgroundDashboardController {
         JSONObject result  = new JSONObject();
         result.putOpt("code", 200);
         JSONObject data = new JSONObject();
+        data.putOpt("user_num", 5);
+        data.putOpt("equip_num", 9);
+        data.putOpt("avatar_num", 14);
+        data.putOpt("map_num", 4);
+        data.putOpt("time_trial_times", 99);
+        data.putOpt("match_times", 44);
         int size = 7;
         data.putOpt("tatal", size);
         JSONArray items = new JSONArray();
         for (int i = 0; i < size; i++){
             JSONObject item = new JSONObject();
             item.putOpt("timestamp", System.currentTimeMillis() - 1000 * 60 * 60 *24 * (7 - i));
-            item.putOpt("data", (i + 1) * 24);
+            item.putOpt("data", (i + 1) * 14);
             items.add(item);
         }
-        data.putOpt("items", items);
+        data.putOpt("login_user_num", items);
+
+        items.clear();
+        for (int i = 0; i < size; i++){
+            JSONObject item = new JSONObject();
+            item.putOpt("timestamp", System.currentTimeMillis() - 1000 * 60 * 60 *24 * (7 - i));
+            item.putOpt("data", (i + 1) * 6);
+            items.add(item);
+        }
+        data.putOpt("time_trial_num", items);
+
+        items.clear();
+        for (int i = 0; i < size; i++){
+            JSONObject item = new JSONObject();
+            item.putOpt("timestamp", System.currentTimeMillis() - 1000 * 60 * 60 *24 * (7 - i));
+            item.putOpt("data", (i + 1) * 8);
+            items.add(item);
+        }
+        data.putOpt("match_num", items);
         result.putOpt("data", data);
         return result;
     }
