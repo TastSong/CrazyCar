7af03e7cd686d15f57869e7d1d82a906bf91c796
TastSong <tastsong@163.com>
2022-07-25 21:18:20 +0800

    set host

--- CrazyCarBackground/src/utils/request.js
+++ CrazyCarBackground/src/utils/request.js
@@ -5,9 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: 'http://localhost:8081/',
-  // baseURL: 'http://139.9.103.145:8081/',
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
