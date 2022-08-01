8dc6f90ea0c729cd45627e2ec75362a36b77df44
TastSong <tastsong@163.com>
2022-08-01 18:57:19 +0800

    add assets updating

---
+++ CarzyCarServer/src/main/java/com/tastsong/crazycar/mapper/AssetsUpdatingMapper.java
@@ -0,0 +1,7 @@
+package com.tastsong.crazycar.mapper;
+
+public interface AssetsUpdatingMapper {
+    public boolean getStatus(Integer id);
+    public String getUrl(Integer id);
+    public long getTime(Integer id);
+}
