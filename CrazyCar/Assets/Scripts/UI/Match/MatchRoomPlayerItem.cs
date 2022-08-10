9109fb3ffaa8e0a084d7afd5858a2acc2038cae8
TastSong <tastsong@163.com>
2022-08-10 16:21:26 +0800

    change default image

--- CrazyCar/Assets/Scripts/UI/Match/MatchRoomPlayerItem.cs
+++ CrazyCar/Assets/Scripts/UI/Match/MatchRoomPlayerItem.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using QFramework;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using Utils;
 
 public class MatchRoomPlayerItem : MonoBehaviour, IController {
     public Text nickName;
+    public GameObject defaultImage;
     public Image avatarImage;
 
     public void SetContent(MatchRoomMemberInfo info) {
         nickName.text = info.memberName;
+        defaultImage.gameObject.SetActiveFast(false);
+        avatarImage.gameObject.SetActiveFast(true);
         this.GetSystem<IAddressableSystem>().GetAvatarResource(info.aid, (obj) => {
             if (obj.Status == AsyncOperationStatus.Succeeded) {
                 avatarImage.sprite = Instantiate(obj.Result, transform, false);
@@ -20,11 +24,8 @@ public class MatchRoomPlayerItem : MonoBehaviour, IController {
 
     public void CleanItem() {
         nickName.text = "等待加入...";
-        this.GetSystem<IAddressableSystem>().GetAvatarResource(10, (obj) => {
-            if (obj.Status == AsyncOperationStatus.Succeeded) {
-                avatarImage.sprite = Instantiate(obj.Result, transform, false);
-            }
-        });
+        defaultImage.gameObject.SetActiveFast(true);
+        avatarImage.gameObject.SetActiveFast(false);
     }
 
     public IArchitecture GetArchitecture() {
