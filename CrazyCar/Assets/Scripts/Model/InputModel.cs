1777cfceb66569b8d1b7734072b85c803545ab39
CherryWhite <1789139001@qq.com>
2022-09-03 20:18:54 +0800

    save keyboard settings

---
+++ CrazyCar/Assets/Scripts/Model/InputModel.cs
@@ -0,0 +1,20 @@
+
+using QFramework;
+
+public interface IInputModel : IModel {
+    BindableProperty<int> IsUseKeyboard { get; }
+}
+
+public class InputModel : AbstractModel, IInputModel 
+{
+    protected override void OnInit() {
+        var storage = this.GetUtility <IPlayerPrefsStorage>();
+        
+        IsUseKeyboard.Value = storage.LoadInt(PrefKeys.isUseKeyboard);
+        IsUseKeyboard.Register(v => { 
+            storage.SaveInt(PrefKeys.isUseKeyboard, v);
+        });
+    }
+
+    public BindableProperty<int> IsUseKeyboard { get; set; } = new();
+}
