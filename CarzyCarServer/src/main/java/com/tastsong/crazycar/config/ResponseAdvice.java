553ea767f81072894e4a08da6540d40e95282ed5
TastSong <tastsong@163.com>
2022-10-24 10:50:22 +0800

    修改异常代码

--- CarzyCarServer/src/main/java/com/tastsong/crazycar/config/ResponseAdvice.java
+++ CarzyCarServer/src/main/java/com/tastsong/crazycar/config/ResponseAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -20,7 +21,7 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
         return true;
     }
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, 
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType, 
         Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response)  {
         if(body instanceof Result){
             return body;
