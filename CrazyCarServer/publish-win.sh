bf7efd631507938c028aafc1ab1c8c2c3d9dba46
TastSong <tastsong@163.com>
2022-10-24 16:37:43 +0800

    重命名server

---
+++ CrazyCarServer/publish-win.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 在windows下使用git bash运行
+mvn clean
+mvn package
+
+rm -rf CrazyCar.zip
+
+mv ./target/CrazyCar.jar CrazyCar.jar
+
+zip CrazyCar.zip CrazyCar.jar
+
+scp CrazyCar.zip huawei:/root/
+
+mv CrazyCar.jar ./target/CrazyCar.jar
+rm -rf CrazyCar.zip
+
+ssh -t huawei "ps -ax | grep CrazyCar | cut -c 1-5  | xargs kill -9"
+ssh -t huawei "rm -rf /root/CrazyCar.jar"
+ssh -t huawei "unzip /root/CrazyCar.zip -d /root/"
+ssh -t huawei "nohup java -jar /root/CrazyCar.jar"
+
+echo "Finish!!!"
+
