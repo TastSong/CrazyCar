85a735f3865f2143dd977c16b4260a5e777ad119
TastSong <tastsong@163.com>
2023-04-17 10:04:25 +0800

    代码格式

--- CrazyCarClient/Assets/Plugins/QFramework/Toolkits/QFSMState/QFSMState.cs
+++ CrazyCarClient/Assets/Plugins/QFramework/Toolkits/QFSMState/QFSMState.cs
@@ -1,70 +1,64 @@
-namespace QFramework
-{
+namespace QFramework {
     using System.Collections.Generic;
- 
+
     /// <summary>
     /// 教程地址:http://liangxiegame.com/post/4/
     /// </summary>
-    public class QFSMLite
-    {
+    public class QFSMLite {
         /// <summary>
         /// FSM callfunc.
         /// </summary>
         public delegate void FSMCallfunc(params object[] param);
- 
+
         /// <summary>
         /// QFSM state.
         /// </summary>
-        class QFSMState
-        {
+        class QFSMState {
             private string mName;
- 
-            public QFSMState(string name)
-            {
+
+            public QFSMState(string name) {
                 mName = name;
             }
- 
+
             /// <summary>
             /// The translation dict.
             /// </summary>
-            public readonly Dictionary<string, QFSMTranslation> TranslationDict = new Dictionary<string, QFSMTranslation>();
+            public readonly Dictionary<string, QFSMTranslation> TranslationDict =
+                new Dictionary<string, QFSMTranslation>();
         }
- 
+
         /// <summary>
         /// Translation 
         /// </summary>
-        public class QFSMTranslation
-        {
+        public class QFSMTranslation {
             public string FromState;
             public string Name;
             public string ToState;
             public FSMCallfunc OnTranslationCallback; // 回调函数
- 
-            public QFSMTranslation(string fromState, string name, string toState, FSMCallfunc onTranslationCallback)
-            {
+
+            public QFSMTranslation(string fromState, string name, string toState, FSMCallfunc onTranslationCallback) {
                 FromState = fromState;
                 ToState = toState;
                 Name = name;
                 OnTranslationCallback = onTranslationCallback;
             }
         }
- 
+
         public string State { get; private set; }
- 
+
         /// <summary>
         /// The m state dict.
         /// </summary>
         private readonly Dictionary<string, QFSMState> mStateDict = new Dictionary<string, QFSMState>();
- 
+
         /// <summary>
         /// Adds the state.
         /// </summary>
         /// <param name="name">Name.</param>
-        public void AddState(string name)
-        {
+        public void AddState(string name) {
             mStateDict[name] = new QFSMState(name);
         }
- 
+
         /// <summary>
         /// Adds the translation.
         /// </summary>
@@ -72,40 +66,35 @@ namespace QFramework
         /// <param name="name">Name.</param>
         /// <param name="toState">To state.</param>
         /// <param name="callfunc">Callfunc.</param>
-        public void AddTranslation(string fromState, string name, string toState, FSMCallfunc callfunc)
-        {
+        public void AddTranslation(string fromState, string name, string toState, FSMCallfunc callfunc) {
             mStateDict[fromState].TranslationDict[name] = new QFSMTranslation(fromState, name, toState, callfunc);
         }
- 
+
         /// <summary>
         /// Start the specified name.
         /// </summary>
         /// <param name="name">Name.</param>
-        public void Start(string name)
-        {
+        public void Start(string name) {
             State = name;
         }
- 
+
         /// <summary>
         /// Handles the event.
         /// </summary>
         /// <param name="name">Name.</param>
         /// <param name="param">Parameter.</param>
-        public void HandleEvent(string name, params object[] param)
-        {
-            if (State != null && mStateDict[State].TranslationDict.ContainsKey(name))
-            {
+        public void HandleEvent(string name, params object[] param) {
+            if (State != null && mStateDict[State].TranslationDict.ContainsKey(name)) {
                 QFSMTranslation tempTranslation = mStateDict[State].TranslationDict[name];
                 tempTranslation.OnTranslationCallback(param);
                 State = tempTranslation.ToState;
             }
         }
- 
+
         /// <summary>
         /// Clear this instance.
         /// </summary>
-        public void Clear()
-        {
+        public void Clear() {
             mStateDict.Clear();
         }
     }
