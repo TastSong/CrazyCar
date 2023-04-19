00e6c453c9b2cc72627df7aa6f4cae316c3a0bfa
TastSong <tastsong@163.com>
2023-04-19 17:17:10 +0800

    自动打包工具修改

--- CrazyCarServer/PublishDatabase.sh
+++ CrazyCarServer/PublishDatabase.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 在windows下使用git bash运行 首先cd 到此脚本所在文件夹即：*/CrazyCar/CrazyCarServer
 
-ssh -t huawei "rm -rf /root/CrazyCar.sql"
-scp ../CrazyCarDB/CrazyCar.sql huawei:/root/
+ssh -t tencent "rm -rf /root/CrazyCar.sql"
+scp ../CrazyCarDB/CrazyCar.sql tencent:/root/
 
 echo "Finish!!!"
 
--- CrazyCarServer/publish-win.sh
+++ CrazyCarServer/publish-win.sh
@@ -9,15 +9,15 @@ mv ./target/crazy_car.jar crazy_car.jar
 
 zip crazy_car.zip crazy_car.jar
 
-scp crazy_car.zip huawei:/root/crazy_car_game/
+scp crazy_car.zip tencent:/root/crazy_car_game/
 
 mv crazy_car.jar ./target/crazy_car.jar
 rm -rf crazy_car.zip
 
-ssh -t huawei "ps -ax | grep crazy_car | cut -c 1-5  | xargs kill -9"
-ssh -t huawei "rm -rf /root/crazy_car_game/crazy_car.jar"
-ssh -t huawei "unzip /root/crazy_car_game/crazy_car.zip -d /root/crazy_car_game/"
-ssh -t huawei "nohup java -jar /root/crazy_car_game/crazy_car.jar >/root/crazy_car_game/crazy_car.txt &"
+ssh -t tencent "ps -ax | grep crazy_car | cut -c 1-5  | xargs kill -9"
+ssh -t tencent "rm -rf /root/crazy_car_game/crazy_car.jar"
+ssh -t tencent "unzip /root/crazy_car_game/crazy_car.zip -d /root/crazy_car_game/"
+ssh -t tencent "nohup java -jar /root/crazy_car_game/crazy_car.jar >/root/crazy_car_game/crazy_car.txt &"
 
 echo "Finish!!!"
 
