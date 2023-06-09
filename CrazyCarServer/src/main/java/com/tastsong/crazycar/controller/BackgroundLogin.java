0f2c30f10ac8ee82f3db8f4cf7b115754e86502a
TastSong <tastsong@163.com>
2023-06-09 18:52:33 +0800

    添加权限测试接口

--- CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundLogin.java
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundLogin.java
@@ -2,6 +2,7 @@ package com.tastsong.crazycar.controller;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ import com.tastsong.crazycar.Util.Util;
 import com.tastsong.crazycar.common.Result;
 import com.tastsong.crazycar.common.ResultCode;
 
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 
 @CrossOrigin
 @RestController
@@ -28,5 +31,35 @@ public class BackgroundLogin {
             return Result.failure(ResultCode.RC404);
         }
         
+    }
+
+
+    @GetMapping(value = "getNav")
+    public Object getNav() throws Exception {
+        String asyncRouters = "[\r\n" + //
+                "  {\r\n" + //
+                "    id: 21,\r\n" + //
+                "    parentId: 0,\r\n" + //
+                "    createTime: '2020-02-07T08:29:13.000+00:00',\r\n" + //
+                "    level: 0,\r\n" + //
+                "    path: '/form',\r\n" + //
+                "    component: 'Layout',\r\n" + //
+                "    redirect: '/form/index',\r\n" + //
+                "    name: 'form',\r\n" + //
+                "    meta: { title: 'form', icon: 'form' },\r\n" + //
+                "    children: [\r\n" + //
+                "      {\r\n" + //
+                "        path: 'form',\r\n" + //
+                "        name: 'form',\r\n" + //
+                "        component: 'form/index',\r\n" + //
+                "        meta: { title: 'form', icon: 'form', roles: ['admin'] }\r\n" + //
+                "      }\r\n" + //
+                "    ]\r\n" + //
+                "  }\r\n" + //
+                "]";
+        JSONArray items = new JSONArray();
+        items = JSONUtil.parseArray(asyncRouters);
+       
+        return items;
     }
 }
