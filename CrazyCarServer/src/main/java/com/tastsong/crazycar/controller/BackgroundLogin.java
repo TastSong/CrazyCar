5083f9ab9708d07716e9a675afb20c11294be3b4
TastSong <tastsong@163.com>
2023-06-10 18:11:43 +0800

    添加权限测试接口

--- CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundLogin.java
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/BackgroundLogin.java
@@ -33,32 +33,143 @@ public class BackgroundLogin {
         
     }
 
-    String asyncRouters = "[\r\n" + //
-                "  {\r\n" + //
-                "    id: 21,\r\n" + //
-                "    parentId: 0,\r\n" + //
-                "    createTime: '2020-02-07T08:29:13.000+00:00',\r\n" + //
-                "    level: 0,\r\n" + //
-                "    path: '/form',\r\n" + //
-                "    component: 'Layout',\r\n" + //
-                "    redirect: '/form/index',\r\n" + //
-                "    name: 'form',\r\n" + //
-                "    meta: { title: 'form', icon: 'form' },\r\n" + //
-                "    children: [\r\n" + //
-                "      {\r\n" + //
-                "        path: 'form',\r\n" + //
-                "        name: 'form',\r\n" + //
-                "        component: 'form/index',\r\n" + //
-                "        meta: { title: 'form', icon: 'form', roles: ['admin'] }\r\n" + //
-                "      }\r\n" + //
-                "    ]\r\n" + //
-                "  }\r\n" + //
-                "]";
+    String asyncRouters = "[\n" + //
+            "    {\n" + //
+            "        \"redirect\":\"/form/index\",\n" + //
+            "        \"level\":0,\n" + //
+            "        \"parentId\":0,\n" + //
+            "        \"path\":\"/form\",\n" + //
+            "        \"component\":\"Layout\",\n" + //
+            "        \"createTime\":\"2020-02-07T08:29:13.000+00:00\",\n" + //
+            "        \"children\":[\n" + //
+            "            {\n" + //
+            "                \"path\":\"form\",\n" + //
+            "                \"component\":\"form/index\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"roles\":[\n" + //
+            "                        \"admin\"\n" + //
+            "                    ],\n" + //
+            "                    \"icon\":\"form\",\n" + //
+            "                    \"title\":\"form\"\n" + //
+            "                },\n" + //
+            "                \"name\":\"form\"\n" + //
+            "            }\n" + //
+            "        ],\n" + //
+            "        \"meta\":{\n" + //
+            "            \"icon\":\"form\",\n" + //
+            "            \"title\":\"form\"\n" + //
+            "        },\n" + //
+            "        \"name\":\"form\",\n" + //
+            "        \"id\":21\n" + //
+            "    }\n" + //
+            "]";
+
+    String allRouters = "[\n" + //
+            "    {\n" + //
+            "        \"redirect\":\"/form/index\",\n" + //
+            "        \"level\":0,\n" + //
+            "        \"parentId\":0,\n" + //
+            "        \"path\":\"/form\",\n" + //
+            "        \"component\":\"Layout\",\n" + //
+            "        \"createTime\":\"2020-02-07T08:29:13.000+00:00\",\n" + //
+            "        \"children\":[\n" + //
+            "            {\n" + //
+            "                \"path\":\"form\",\n" + //
+            "                \"component\":\"form/index\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"roles\":[\n" + //
+            "                        \"admin\"\n" + //
+            "                    ],\n" + //
+            "                    \"icon\":\"form\",\n" + //
+            "                    \"title\":\"form\"\n" + //
+            "                },\n" + //
+            "                \"name\":\"form\"\n" + //
+            "            }\n" + //
+            "        ],\n" + //
+            "        \"meta\":{\n" + //
+            "            \"icon\":\"form\",\n" + //
+            "            \"title\":\"form\"\n" + //
+            "        },\n" + //
+            "        \"name\":\"form\",\n" + //
+            "        \"id\":21\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "        \"path\":\"/permission\",\n" + //
+            "        \"component\":\"layout/Layout\",\n" + //
+            "        \"redirect\":\"/permission/index\",\n" + //
+            "        \"alwaysShow\":true,\n" + //
+            "        \"meta\":{\n" + //
+            "            \"title\":\"Permission\",\n" + //
+            "            \"icon\":\"lock\",\n" + //
+            "            \"roles\":[\n" + //
+            "                \"admin\",\n" + //
+            "                \"editor\"\n" + //
+            "            ]\n" + //
+            "        },\n" + //
+            "        \"children\":[\n" + //
+            "            {\n" + //
+            "                \"path\":\"page\",\n" + //
+            "                \"component\":\"views/permission/page\",\n" + //
+            "                \"name\":\"PagePermission\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"title\":\"Page Permission\",\n" + //
+            "                    \"roles\":[\n" + //
+            "                        \"admin\"\n" + //
+            "                    ]\n" + //
+            "                }\n" + //
+            "            },\n" + //
+            "            {\n" + //
+            "                \"path\":\"directive\",\n" + //
+            "                \"component\":\"views/permission/directive\",\n" + //
+            "                \"name\":\"DirectivePermission\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"title\":\"Directive Permission\"\n" + //
+            "                }\n" + //
+            "            },\n" + //
+            "            {\n" + //
+            "                \"path\":\"role\",\n" + //
+            "                \"component\":\"views/permission/role\",\n" + //
+            "                \"name\":\"RolePermission\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"title\":\"Role Permission\",\n" + //
+            "                    \"roles\":[\n" + //
+            "                        \"admin\"\n" + //
+            "                    ]\n" + //
+            "                }\n" + //
+            "            }\n" + //
+            "        ]\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "        \"path\":\"/icon\",\n" + //
+            "        \"component\":\"layout/Layout\",\n" + //
+            "        \"children\":[\n" + //
+            "            {\n" + //
+            "                \"path\":\"index\",\n" + //
+            "                \"component\":\"views/icons/index\",\n" + //
+            "                \"name\":\"Icons\",\n" + //
+            "                \"meta\":{\n" + //
+            "                    \"title\":\"Icons\",\n" + //
+            "                    \"icon\":\"icon\",\n" + //
+            "                    \"noCache\":true\n" + //
+            "                }\n" + //
+            "            }\n" + //
+            "        ]\n" + //
+            "    }\n" + //
+            "]";            
 
     @GetMapping(value = "getNav")
     public Object getNav() throws Exception {
         JSONArray items = new JSONArray();
         items = JSONUtil.parseArray(asyncRouters);
+       
+        return items;
+    }
+
+    @GetMapping(value = "getAllRoutes")
+    public Object getAllRoutes() throws Exception {
+        
+        JSONArray items = new JSONArray();
+        items = JSONUtil.parseArray(allRouters);
        
         return items;
     }
