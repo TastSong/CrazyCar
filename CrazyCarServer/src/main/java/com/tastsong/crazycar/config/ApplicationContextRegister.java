cf15c28df3d6a43becfb8833291e68b60160070c
TastSong <tastsong@163.com>
2023-09-15 13:42:13 +0800

    log

--- CrazyCarServer/src/main/java/com/tastsong/crazycar/config/ApplicationContextRegister.java
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/config/ApplicationContextRegister.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+// 通过这个类，你可以在任何地方获取到ApplicationContext对象，进而访问和操作Spring容器中的Bean
 @Component
 @Lazy(false)
 public class ApplicationContextRegister  implements ApplicationContextAware {
