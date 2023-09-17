a24c848298593eca6f564143946fa837ad61c8dc
TastSong <tastsong@163.com>
2023-09-17 17:25:44 +0800

    添加错误提示

--- CrazyCarBackground/src/utils/request.js
+++ CrazyCarBackground/src/utils/request.js
@@ -48,7 +48,7 @@ service.interceptors.response.use(
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 200) {
       Message({
-        message: res.message || 'Error',
+        message: res.msg || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
