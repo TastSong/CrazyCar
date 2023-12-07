8db406c72be11df35ea43c2e0773b897dba2dbb5
TastSong <tastsong@163.com>
2023-12-07 15:23:00 +0800

    æ增加请求合规配置

---
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/config/GlobalExceptionConfig.java
@@ -0,0 +1,34 @@
+package com.tastsong.crazycar.config;
+
+
+import com.tastsong.crazycar.common.Result;
+import com.tastsong.crazycar.common.ResultCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionConfig {
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleValidationException(BindException ex) {
+        // 从异常中获取所有验证错误
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        // 处理验证错误，这里简单示例，你可以根据需要定制错误信息
+        List<String> errorMessages = fieldErrors.stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        // 返回自定义的错误信息
+        return Result.failure(ResultCode.RC400, errorMessages.toString());
+    }
+}
