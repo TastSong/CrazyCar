c9e2e944f2c7e4c4fdcc072b510b5e5f09a4092c
TastSong <tastsong@163.com>
2023-12-18 09:41:24 +0800

    game info

---
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/dto/req/ReqEnterGame.java
@@ -0,0 +1,26 @@
+package com.tastsong.crazycar.dto.req;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+
+public class ReqEnterGame {
+    @NotNull
+    @Min(0)
+    private int cid;
+    @JsonProperty("GameType")
+    private int GameType;
+    @JsonProperty("NetType")
+    private int NetType;
+}
