161b2d366ad5421a945ba4ceddee52074db12f39
TastSong <tastsong@163.com>
2023-12-18 09:24:50 +0800

    change equip

--- CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/EquipController.java
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/controller/EquipController.java
@@ -53,20 +53,14 @@ public class EquipController {
     }
 
     @PostMapping(value = "/Change")
-    public Object changeEquip(@RequestHeader(Util.TOKEN) String token, @RequestBody JSONObject body) throws Exception{
+    public Object changeEquip(@RequestHeader(Util.TOKEN) String token, @Valid @RequestBody ReqEquip req) throws Exception{
         int uid = Util.getUidByToken(token);
-        if (body != null && body.containsKey("eid")) {
-			int eid = body.getInt("eid");
-			if (equipService.hasEquip(uid, eid)) {
-				equipService.changeEquip(uid, eid);
-                JSONObject data = new JSONObject();
-                data.putOpt("eid", eid);
-                return data;
-			} else {
-                return Result.failure(ResultCode.RC423);
-			}
-		} else {
-            return Result.failure(ResultCode.RC404);
-		}	
+        int eid = req.getEid();
+        if (equipService.hasEquip(uid, eid)) {
+            equipService.changeEquip(uid, eid);
+            return userService.getUserByUid(uid);
+        } else {
+            return Result.failure(ResultCode.RC423, "未拥有该装备");
+        }
     }
 }
