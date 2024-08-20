980982849c17459bff5226a8dcc0107ad129df27
TastSong <tastsong@163.com>
2024-08-20 13:45:42 +0800

    优化loading time

--- CrazyCarClient/Assets/Scripts/Event/HidePageByLevelEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿
-public class HidePageByLevelEvent {
-    public UILevelType mLevelType;
-    
-    public HidePageByLevelEvent(UILevelType levelType) {
-        this.mLevelType = levelType;
-    }
-}
--- CrazyCarClient/Assets/Scripts/Game/ShowPageInfo.cs
+++ CrazyCarClient/Assets/Scripts/Game/ShowPageInfo.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 using UnityEngine;
-public class ShowPageEvent {
+public class ShowPageInfo {
     public UIPageType pageType;
     public bool closeOther;
     public UILevelType levelType;
     public object data;
 
-    public ShowPageEvent(UIPageType pageType, bool closeOther = false, UILevelType levelType = UILevelType.UIPage) {
+    public ShowPageInfo(UIPageType pageType, bool closeOther = false, UILevelType levelType = UILevelType.UIPage) {
         this.pageType = pageType;
         this.closeOther = closeOther;
         this.levelType = levelType;
         this.data = null;
     }
     
-    public ShowPageEvent(UIPageType pageType, UILevelType levelType, object data) {
+    public ShowPageInfo(UIPageType pageType, UILevelType levelType, object data) {
         this.pageType = pageType;
         this.closeOther = false;
         this.levelType = levelType;
         this.data = data;
     }
     
-    public ShowPageEvent(UIPageType pageType, UILevelType levelType) {
+    public ShowPageInfo(UIPageType pageType, UILevelType levelType) {
         this.pageType = pageType;
         this.closeOther = false;
         this.levelType = levelType;
         this.data = null;
     }
 
-    public ShowPageEvent(UIPageType pageType, bool closeOther, UILevelType levelType, object data) {
+    public ShowPageInfo(UIPageType pageType, bool closeOther, UILevelType levelType, object data) {
         this.pageType = pageType;
         this.closeOther = false;
         this.levelType = levelType;
