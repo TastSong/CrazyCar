28c64c1405a7474a06550bd006cc0ea2802e6f16
TastSong <tastsong@163.com>
2024-08-29 16:44:05 +0800

    完成切换头像

--- CrazyCarClient/Assets/Scripts/Command/ApplyAvatarCommand.cs
+++ CrazyCarClient/Assets/Scripts/Command/ApplyAvatarCommand.cs
@@ -10,7 +10,7 @@ public class ApplyAvatarCommand : AbstractCommand {
         mCurAid = curAid;
     }
 
-    protected override void OnExecute() {
+    protected override async void OnExecute() {
         StringBuilder sb = new StringBuilder();
         JsonWriter w = new JsonWriter(sb);
         w.WriteObjectStart();
@@ -19,19 +19,16 @@ public class ApplyAvatarCommand : AbstractCommand {
         w.WriteObjectEnd();
         Debug.Log("++++++ " + sb.ToString());
         byte[] bytes = Encoding.UTF8.GetBytes(sb.ToString());
-        CoroutineController.Instance.StartCoroutine(this.GetSystem<INetworkSystem>().POSTHTTP(url: this.GetSystem<INetworkSystem>().HttpBaseUrl + RequestUrl.changeAvatarUrl,
-            data: bytes, token: this.GetModel<IGameModel>().Token.Value,
-            succData: (data) => {
-                this.GetModel<IUserModel>().Aid.Value = (int)data["aid"];
-                this.SendEvent(new UpdataAvatarUIEvent(this.GetModel<IUserModel>().Aid));
-                WarningAlertInfo alertInfo = new WarningAlertInfo("Successfully Set");
-                UIController.Instance.ShowPage(new ShowPageInfo(UIPageType.WarningAlert, UILevelType.Alart, alertInfo));
-            },
-            code: (code) => {
-                if (code == 423) {
-                    WarningAlertInfo alertInfo = new WarningAlertInfo("Did not have");
-                    UIController.Instance.ShowPage(new ShowPageInfo(UIPageType.WarningAlert, UILevelType.Alart, alertInfo));
-                }
-            }));
+        var result = await this.GetSystem<INetworkSystem>().Post(
+            url: this.GetSystem<INetworkSystem>().HttpBaseUrl + RequestUrl.changeAvatarUrl, this.GetModel<IGameModel>().Token.Value, bytes);
+        if (result.serverCode == 200) {
+            this.GetModel<IUserModel>().Aid.Value = (int)result.serverData["aid"];
+            this.SendEvent(new UpdataAvatarUIEvent(this.GetModel<IUserModel>().Aid));
+            WarningAlertInfo alertInfo = new WarningAlertInfo("Successfully Set");
+            UIController.Instance.ShowPage(new ShowPageInfo(UIPageType.WarningAlert, UILevelType.Alart, alertInfo));
+        } else if (result.serverCode == 423) {
+            WarningAlertInfo alertInfo = new WarningAlertInfo("Did not have");
+            UIController.Instance.ShowPage(new ShowPageInfo(UIPageType.WarningAlert, UILevelType.Alart, alertInfo));
+        }   
     }
 }
